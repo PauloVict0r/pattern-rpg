e92efb112656835f5987f2c6e2850a7cbe5bea6a
j0sevictor <victorlimaf55@gmail.com>
2026-03-23 21:11:20 -0300

    feat: decorator e strategy

--- PatternRPG/src/main/java/org/pattern/rpg/domain/entity/Entity.java
+++ PatternRPG/src/main/java/org/pattern/rpg/domain/entity/Entity.java
@@ -1,4 +1,10 @@
 package org.pattern.rpg.domain.entity;
 
 public interface Entity {
+    int getAttack();
+    int getHP();
+    int getDefense();
+    double getCriticalChance();
+    int attack(Entity target);
+    int receiveDamage(int damage);
 }
--- PatternRPG/src/main/java/org/pattern/rpg/domain/item/wearable/WearableDecorator.java
+++ PatternRPG/src/main/java/org/pattern/rpg/domain/item/wearable/WearableDecorator.java
@@ -1,4 +1,48 @@
 package org.pattern.rpg.domain.item.wearable;
 
-public class WearableDecorator {
+import org.pattern.rpg.domain.entity.Entity;
+import org.pattern.rpg.domain.item.Item;
+import org.pattern.rpg.domain.stats.StatsBonus;
+
+public abstract class WearableDecorator implements Entity, Item {
+    private final Entity wrappedEntity;
+    private final StatsBonus bonus;
+
+    public WearableDecorator(Entity wrappedEntity,  StatsBonus bonus) {
+        this.wrappedEntity = wrappedEntity;
+        this.bonus = bonus;
+    }
+
+    public StatsBonus getBonus() {
+        return bonus;
+    }
+
+    @Override
+    public int getAttack() {
+        return wrappedEntity.getAttack() + bonus.attack();
+    }
+
+    @Override
+    public int getDefense() {
+        return wrappedEntity.getDefense() + bonus.defense();
+    }
+
+    @Override
+    public int getHP() {
+        return wrappedEntity.getHP() + bonus.hp();
+    }
+
+    @Override
+    public double getCriticalChance() {
+        return wrappedEntity.getCriticalChance() + bonus.critChance();
+    }
+
+    public int attack(Entity target) {
+        return wrappedEntity.attack(target);
+    }
+
+    public int receiveDamage(int damage) {
+        return wrappedEntity.receiveDamage(damage);
+    }
+
 }
