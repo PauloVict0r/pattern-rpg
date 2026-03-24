c5380ee832da53fe3cdf4552d692739df35e8ea8
j0sevictor <victorlimaf55@gmail.com>
2026-03-23 21:38:08 -0300

    refactor: main class name

--- PatternRPG/src/main/java/org/pattern/rpg/Main.java
+++ PatternRPG/src/main/java/org/pattern/rpg/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 import java.io.IOException;
 import java.util.Random;
 
-public class main {
+public class Main {
 
     public static void main(String[] args) {
         LimparTerminal.limparTerminal();
