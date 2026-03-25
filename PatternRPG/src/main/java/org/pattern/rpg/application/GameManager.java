c2d314c7eaa57ff08f2d2bf1218d39374315ac35
Victor Garcia <cicerovictor0903@gmail.com>
2026-03-25 18:36:39 -0300

    Add files via upload

--- PatternRPG/src/main/java/org/pattern/rpg/application/GameManager.java
+++ PatternRPG/src/main/java/org/pattern/rpg/application/GameManager.java
@@ -1,54 +1,99 @@
 package org.pattern.rpg.application;
 
+import org.pattern.rpg.domain.entity.Save;
+import org.pattern.rpg.infrastructure.database.SaveRepository;
 import org.pattern.rpg.presentation.menu.Menu;
 import org.pattern.rpg.presentation.ui.ConsoleUI;
+
+import java.util.List;
 import java.util.Scanner;
 
 public class GameManager {
+
     private Scanner scanner;
     private ConsoleUI ui;
     private Menu menu;
     private BattleManager battleManager;
-    private InventoryManager inventoryManager; // Adicionado
-    private boolean jogoRodando;
+    private InventoryManager inventoryManager;
+    private SaveRepository saveRepository;
+    private boolean isRunning;
+
+    private int currentFloor;
+    private int currentScore;
+    private String[] currentItems;
 
     public GameManager() {
-        this.scanner = new Scanner(System.in);
-        this.ui = new ConsoleUI(scanner);
-        this.menu = new Menu(ui, this);
-        this.battleManager = new BattleManager(ui);
-        this.inventoryManager = new InventoryManager(ui); // Inicializado
+        scanner = new Scanner(System.in);
+        ui = new ConsoleUI(scanner);
+        menu = new Menu(ui, this);
+        battleManager = new BattleManager(ui);
+        inventoryManager = new InventoryManager(ui);
+        saveRepository = new SaveRepository();
+
+        currentFloor = 1;
+        currentScore = 0;
+        currentItems = new String[]{"Sword", "Potion"};
     }
 
-    public void iniciarAplicacao() {
-        this.jogoRodando = true;
-        while (jogoRodando) {
-            menu.exibirMenuPrincipal();
+    public void startApplication() {
+        isRunning = true;
+        while (isRunning) {
+            menu.showMainMenu();
         }
-        scanner.close();
     }
 
-    public void encerrarJogo() {
-        this.jogoRodando = false;
+    public void stopGame() {
+        isRunning = false;
     }
 
-    public void orquestrarNovoJogo(String nomeJogador, String equipamentoEscolhido) {
-        // FUTURAMENTE: 
-        // 1. Criar o Player usando PlayerBuilder: Player p = new PlayerBuilder().withName(nomeJogador)...
-        // 2. Passar o objeto Player para o BattleManager e para o fim de jogo
-        
-        // Chamando a Batalha
-        battleManager.iniciarBatalhaTeste(nomeJogador, equipamentoEscolhido);
-        
-        // Chamando o Fim de Jogo logo após a batalha, exatamente como no seu código original
-        menu.exibirFimDeJogo(nomeJogador, equipamentoEscolhido);
+    public void startNewGame(String name, String equipment) {
+
+        currentFloor = 1;
+        currentScore = 100;
+        currentItems = new String[]{equipment, "Potion"};
+
+        battleManager.startTestBattle(name, equipment);
+
+        currentFloor = 2;
+        currentScore = 250;
+
+        menu.showGameOver(name, equipment);
     }
 
-    public void continuarJogo() {
-        ui.limparTerminal();
-        // FUTURAMENTE:
-        // Aqui usaremos ConnectionDB e SaveRepository para carregar o estado do jogo.
-        // Por enquanto, apenas abrimos o inventário estático como no case 1 original.
-        inventoryManager.mostrarInventario(100, 1000, "Mahoraga");
+    public void continueGame() {
+        inventoryManager.showInventory(100, currentScore, "Player");
     }
+
+    // SAVE
+
+    public List<Save> listSaves() {
+        return saveRepository.listAllSaves();
+    }
+
+    public Save loadSave(int id) {
+        return saveRepository.loadSave(id);
+    }
+
+    public void applyLoadedSave(Save save) {
+        if (save == null) return;
+
+        currentFloor = save.getFloor();
+        currentScore = save.getScore();
+        currentItems = save.getItems();
+    }
+
+    public void createSave() {
+        saveRepository.createSave(new Save(currentItems, currentFloor, currentScore));
+    }
+
+    public void overwriteSave(int id) {
+        saveRepository.overwriteSave(new Save(id, currentItems, currentFloor, currentScore));
+    }
+
+    public void deleteSave(int id) {
+        saveRepository.deleteSave(id);
+    }
+
+    public int getCurrentFloor() { return currentFloor; }
+    public int getCurrentScore() { return currentScore; }
 }
