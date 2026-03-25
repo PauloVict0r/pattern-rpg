c1cf550de9946faab49543fd234cc0bb1ea0c7f9
j0sevictor <victorlimaf55@gmail.com>
2026-03-25 20:17:19 -0300

    fix: singleton criando muitiplas conexões

--- PatternRPG/src/main/java/org/pattern/rpg/infrastructure/database/ConnectionDB.java
+++ PatternRPG/src/main/java/org/pattern/rpg/infrastructure/database/ConnectionDB.java
@@ -5,12 +5,12 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionDB {
-
     private static ConnectionDB instance;
-
     private static final String URL = "jdbc:postgresql://localhost:5432/game_saves";
     private static final String USER = "postgres";
     private static final String PASSWORD = "142536";
+
+    private Connection connection;
 
     private ConnectionDB() {}
 
@@ -22,12 +22,15 @@ public class ConnectionDB {
     }
 
     public Connection getConnection() {
-        try {
-            return DriverManager.getConnection(URL, USER, PASSWORD);
-        } catch (SQLException e) {
-            System.out.println("Database connection error:");
-            System.out.println(e.getMessage());
-            return null;
+        if (connection == null) {
+            try {
+                connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            } catch (SQLException e) {
+                System.out.println("Database connection error:");
+                System.out.println(e.getMessage());
+                return null;
+            }
         }
+        return connection;
     }
 }
