c037c45b6d00d15ba85c4c4d87102f07cfe75067
Victor Garcia <cicerovictor0903@gmail.com>
2026-03-25 18:33:55 -0300

    Add files via upload

---
+++ PatternRPG/target/classes/org/pattern/rpg/infrastructure/database/SaveRepository.java
@@ -0,0 +1,130 @@
+package org.pattern.rpg.infrastructure.database;
+
+import org.pattern.rpg.domain.entity.Save;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SaveRepository {
+
+    public void createSave(Save save) {
+        String sql = "INSERT INTO saves (item, pfloor, score) VALUES (?, ?, ?)";
+
+        Connection connection = ConnectionDB.getInstance().getConnection();
+
+        try (connection; PreparedStatement stmt = connection.prepareStatement(sql)) {
+
+            stmt.setArray(1, connection.createArrayOf("text", save.getItems()));
+            stmt.setInt(2, save.getFloor());
+            stmt.setInt(3, save.getScore());
+
+            stmt.executeUpdate();
+            System.out.println("Save created!");
+
+        } catch (SQLException e) {
+            System.out.println("Error creating save:");
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void overwriteSave(Save save) {
+        String sql = "UPDATE saves SET item=?, pfloor=?, score=? WHERE id=?";
+
+        Connection connection = ConnectionDB.getInstance().getConnection();
+
+        try (connection; PreparedStatement stmt = connection.prepareStatement(sql)) {
+
+            stmt.setArray(1, connection.createArrayOf("text", save.getItems()));
+            stmt.setInt(2, save.getFloor());
+            stmt.setInt(3, save.getScore());
+            stmt.setInt(4, save.getId());
+
+            stmt.executeUpdate();
+            System.out.println("Save overwritten!");
+
+        } catch (SQLException e) {
+            System.out.println("Error overwriting save:");
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public Save loadSave(int id) {
+        String sql = "SELECT * FROM saves WHERE id=?";
+
+        Connection connection = ConnectionDB.getInstance().getConnection();
+
+        try (connection; PreparedStatement stmt = connection.prepareStatement(sql)) {
+
+            stmt.setInt(1, id);
+
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                int floor = rs.getInt("pfloor");
+                int score = rs.getInt("score");
+
+                Array array = rs.getArray("item");
+                String[] items = (String[]) array.getArray();
+
+                return new Save(id, items, floor, score);
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Error loading save:");
+            System.out.println(e.getMessage());
+        }
+
+        return null;
+    }
+
+    public List<Save> listAllSaves() {
+
+        List<Save> saves = new ArrayList<>();
+
+        String sql = "SELECT * FROM saves ORDER BY id";
+
+        Connection connection = ConnectionDB.getInstance().getConnection();
+
+        try (connection; PreparedStatement stmt = connection.prepareStatement(sql);
+             ResultSet rs = stmt.executeQuery()) {
+
+            while (rs.next()) {
+
+                int id = rs.getInt("id");
+                int floor = rs.getInt("pfloor");
+                int score = rs.getInt("score");
+
+                Array array = rs.getArray("item");
+                String[] items = (String[]) array.getArray();
+
+                saves.add(new Save(id, items, floor, score));
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Error listing saves:");
+            System.out.println(e.getMessage());
+        }
+
+        return saves;
+    }
+
+    public void deleteSave(int id) {
+
+        String sql = "DELETE FROM saves WHERE id=?";
+
+        Connection connection = ConnectionDB.getInstance().getConnection();
+
+        try (connection; PreparedStatement stmt = connection.prepareStatement(sql)) {
+
+            stmt.setInt(1, id);
+            stmt.executeUpdate();
+
+            System.out.println("Save deleted!");
+
+        } catch (SQLException e) {
+            System.out.println("Error deleting save:");
+            System.out.println(e.getMessage());
+        }
+    }
+}
